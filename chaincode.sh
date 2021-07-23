98fab3cecf62079a73a40840afccbbb214730416
hansrajrami <hansrajrami5@gmail.com>
2021-07-23 23:49:01 +0530

    added core.yaml config

--- chaincode.sh
+++ chaincode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export FABRIC_LOGGING_SPEC=DEBUG
+export FABRIC_LOGGING_SPEC=INFO
 export CORE_PEER_LOCALMSPID=DEFAULT
 export CORE_PEER_MSPCONFIGPATH="$PWD/msp"
 export CORE_PEER_TLS_ENABLED=false
-export CORE_CHAINCODE_ID_NAME=$1:$2
+export CORE_CHAINCODE_ID_NAME="$1:0"
 export CHAINCODE_ADDR="$(docker container inspect peer -f '{{.NetworkSettings.Networks.dev_chaincode.IPAddress}}'):7052"
 
